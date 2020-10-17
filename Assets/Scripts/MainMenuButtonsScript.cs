0ec6dce771e662ffa63801e5fa1cd8ce864622e9
aleenaloves <khan.aleena1@gmail.com>
2020-10-17 15:00:14 -0500

    subtract player health + global variables

--- Assets/Scripts/MainMenuButtonsScript.cs
+++ Assets/Scripts/MainMenuButtonsScript.cs
@@ -6,6 +6,6 @@ using UnityEngine.SceneManagement;
 public class MainMenuButtonsScript : MonoBehaviour
 {
     public void PlayGame () {
-        SceneManager.LoadScene(0); //will have to change this with where current player last saved if already started game
+        SceneManager.LoadScene(2); //will have to change this with where current player last saved if already started game
     }
 }
