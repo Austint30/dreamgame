f25ccf99c0a2ea80fd7dba4952ba959f59c2f886
aleenaloves <khan.aleena1@gmail.com>
2020-10-23 19:42:08 -0500

    Dialogue System + Sound Manager
    Added in fully customizeable dialogue system + the starts to the sound manager :)

---
+++ Assets/Scripts/DialogueSystem/DialogueHolder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueHolder : MonoBehaviour
+    {
+        private void Awake()
+        {
+            StartCoroutine(dialogueSequence());
+        }
+
+        private IEnumerator dialogueSequence()
+        {
+            for(int i = 0; i < transform.childCount; i++){
+                Deactivate();
+                transform.GetChild(i).gameObject.SetActive(true);
+                yield return new WaitUntil(() => transform.GetChild(i).GetComponent<DialogueLine>().finished);
+            }
+        }
+
+        private void Deactivate(){
+            for(int i = 0; i < transform.childCount; i++){
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }    
+    }
+}
+
