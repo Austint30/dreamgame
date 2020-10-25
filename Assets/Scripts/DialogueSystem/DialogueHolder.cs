71ff51a1dedbb3197e202615eb9d60a428cfc319
aleenaloves <khan.aleena1@gmail.com>
2020-10-25 14:34:06 -0500

    Made triggers for dialogues.

--- Assets/Scripts/DialogueSystem/DialogueHolder.cs
+++ Assets/Scripts/DialogueSystem/DialogueHolder.cs
@@ -7,9 +7,28 @@ namespace DialogueSystem
 {
     public class DialogueHolder : MonoBehaviour
     {
+
+        [Header ("Active Settings")]
+        [SerializeField] public bool activeOnStart;
+        [Tooltip ("If active on start, then character tag not necessary")]
+        [SerializeField] public string characterTag;    
+
         private void Awake()
         {
-            StartCoroutine(dialogueSequence());
+            gameObject.SetActive(activeOnStart);
+
+            if(activeOnStart == true){
+                StartCoroutine(dialogueSequence());
+            }
+        }
+
+        void OnTriggerStay(Collider other){
+            
+            //Can change keycode to whatever
+            if(other.tag == characterTag && Input.GetKeyDown(KeyCode.A)){
+                gameObject.SetActive(true);
+                StartCoroutine(dialogueSequence());
+            }
         }
 
         private IEnumerator dialogueSequence()
@@ -29,4 +48,46 @@ namespace DialogueSystem
         }    
     }
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// namespace DialogueSystem
+// {
+//     public class DialogueHolder : MonoBehaviour
+//     {
+
+//         [Header ("Active Settings")]
+//         [SerializeField] private bool activeOnStart;
+
+//         void OnTriggerStay(Collider other){
+//             if(other.tag == "characterTagName" && Input.GetKeyDown(KeyCode.A)){
+//                 gameObject.SetActive(true);
+//                 StartCoroutine(dialogueSequence());
+//             }
+//         }
+
+//         private void Awake(){
+//             gameObject.SetActive(activeOnStart);
+//         }
+
+//         private IEnumerator dialogueSequence()
+//         {
+//             for(int i = 0; i < transform.childCount; i++){
+//                 Deactivate();
+//                 transform.GetChild(i).gameObject.SetActive(true);
+//                 yield return new WaitUntil(() => transform.GetChild(i).GetComponent<DialogueLine>().finished);
+//             }
+//             gameObject.SetActive(false);
+//         }
+
+//         private void Deactivate(){
+//             for(int i = 0; i < transform.childCount; i++){
+//                 transform.GetChild(i).gameObject.SetActive(false);
+//             }
+//         }    
+//     }
+// }
 
