9ec1a0adb4debb0206caf028fa26cd564b4c1629
Austin <austinthibodeaux@gmail.com>
2020-11-01 22:12:33 -0600

    Added powerup animation and other stuff

--- Assets/Scripts/Powerup/AbstractPowerup.cs
+++ Assets/Scripts/Powerup/AbstractPowerup.cs
@@ -7,8 +7,9 @@ public abstract class AbstractPowerup : MonoBehaviour
     [Tooltip("The sprite used when the player hasn't collected the powerup yet. Uncollected sprite is made invisible upon initialization.")]
     public SpriteRenderer uncollectedSprite;
     public virtual void Initialize(){
-        if (uncollectedSprite != null){
-            uncollectedSprite.enabled = false; // Disable sprite visible when not collected by player
+        Animator animator = uncollectedSprite.GetComponent<Animator>();
+        if (uncollectedSprite != null && uncollectedSprite.transform.GetComponent<AbstractPowerup>() == null){ // Ensure we are not disabling the gameobject that the powerup script is connected to.
+            uncollectedSprite.gameObject.SetActive(false); // Disable sprite visible when not collected by player.
         }
     }
 }
