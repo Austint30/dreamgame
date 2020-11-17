a257c93559492a95b5a75a5ea8f666c634304c81
Austin Thibodeaux <austinthibodeaux@gmail.com>
2020-11-17 17:02:18 -0600

    Character will no longer think other trigger colliders are walls

--- Assets/Scripts/Player/WallCheck.cs
+++ Assets/Scripts/Player/WallCheck.cs
@@ -8,10 +8,12 @@ public class WallCheck : MonoBehaviour
     public Player playerScript;
 
     private void OnTriggerEnter2D(Collider2D other) {
+        if (other.isTrigger) return; // Don't react to other triggers
         playerScript.OnWallCheckTriggerEnter(other);
     }
 
     private void OnTriggerExit2D(Collider2D other) {
+        if (other.isTrigger) return; // Don't react to other triggers
         playerScript.OnWallCheckTriggerExit(other);
     }
 }
