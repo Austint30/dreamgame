df2ffe3c9dad3cae8442a823a822806f95d4fcd3
Poopyman237 <55521747+Poopyman237@users.noreply.github.com>
2020-11-17 13:34:56 -0600

    invisible paltform
    invisible sript

---
+++ Assets/Scripts/Invisibility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invisibility : MonoBehaviour
+{
+
+    SpriteRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            rend.enabled = !rend.enabled;
+        }
+    }
+}
